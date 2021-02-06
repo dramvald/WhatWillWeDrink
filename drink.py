a165f0fb8e6f851bd4d30fbf42d3d555f51f9ad9
Dramvald <kirillpior@gmail.com>
2021-02-06 17:09:24 +0300

    the third

--- drink.py
+++ drink.py
@@ -1,12 +1,14 @@
 import requests
 import json
-import tableprint as tp
-import numpy as np
 import traceback
+from prettytable import PrettyTable
+p = PrettyTable()
 
 try:
     # req_url адрес сервера
     req_url = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+
+
     def reqs(req_url):
         # Делам запрос на сервер по адресу req_url.
         resp = requests.get(req_url).text
@@ -24,24 +26,38 @@ try:
                           item['strIngredient7'], item['strIngredient8'], item['strIngredient9'], \
                           item['strIngredient10'], item['strIngredient11'], item['strIngredient12'], \
                           item['strIngredient13'], item['strIngredient14'], item['strIngredient15']
+            measure = item['strMeasure1'], item['strMeasure2'], item['strMeasure3'], \
+                      item['strMeasure4'], item['strMeasure5'], item['strMeasure6'], \
+                      item['strMeasure7'], item['strMeasure8'], item['strMeasure9'], \
+                      item['strMeasure10'], item['strMeasure11'], item['strMeasure12'], \
+                      item['strMeasure13'], item['strMeasure14'], item['strMeasure15']
+            # уменьшаю список убирая в ingredients и measure элементы имеющие None
+            ingredients = list(filter(None, ingredients))
+            measure = list(filter(None, measure))
             # делаю проверку на алкогольный или без алкогольный напиток, и если он без алкогольный,
             # то отправляеться сново запрос и проходит по циклу
             if alcohol != 'Alcoholic':
                 print(alcohol)
-                print(tp.header(['Назваие напитка', 'Инструкиця по приготовлению', 'Ингридиеты'], ))
-                print(tp.row([drink, instruction, str(ingredients)], width=10))
+                p.add_column('Назваие напитка', [drink])
+                p.add_column('Инструкиця по приготовлению', [instruction])
+                p.add_column('Ингридиеты', [ingredients])
+                p.add_column('Колливечто ингридиента', [measure])
+                print(p.get_string())
                 return reqs(req_url)
             else:
                 print(alcohol)
-                print(tp.header(['Назваие напитка', 'Инструкиця по приготовлению', 'Ингридиеты'], ))
-                print(tp.row([drink, instruction, str(ingredients)], width=10))
+                p.add_column('Назваие напитка', [drink])
+                p.add_column('Инструкиця по приготовлению', [instruction])
+                p.add_column('Ингридиеты', [ingredients])
+                p.add_column('Колливечто ингридиента', [measure])
+                print(p.get_string())
 
 
     reqs(req_url)
-# сдесь работаем с ошибмой если нет отсутсвует интернет
+# здесь работа над ошибмой, когда отсутсвует интернет
 except ConnectionError:
     print('Нет соединения с интернетом')
-# сдесь работаем со всеми остальными ошибками которые возникают в ходе программы
+# здесь работа над всеми остальными ошибками которые возникают в ходе программы
 except Exception as e:
     print('Ошибка:\n', traceback.format_exc())
     print()
