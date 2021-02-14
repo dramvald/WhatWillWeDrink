8567436bcd14afafed8313ca89b2b9b903fbffb3
Dramvald <kirillpior@gmail.com>
2021-02-14 22:36:09 +0300

    corrected main.py

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import drink
 import requests
 
 RANDOM_DRINK_API_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+INGREDIENT = 'strIngredient'
+MEASURE = 'strMeasure'
+
 try:
-    tipple, instruction, ingredients, measure = drink.get_drink_data(RANDOM_DRINK_API_URL)
-    drink.show_drink(tipple, instruction, ingredients, measure)
+    drinks, instruction, data_drinks = drink.get_drink_data(RANDOM_DRINK_API_URL)
+    ingredients = drink.sort_values(data_drinks, INGREDIENT)
+    measure = drink.sort_values(data_drinks, MEASURE)
+    drink.show_drink(drinks, instruction, ingredients, measure)
 # Здесь работа над ошибкой, когда отсутствует интернет.
 except ConnectionError:
     print('No internet connection!')
