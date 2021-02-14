8b2f25104c4a23138cb34e7adbbd83a6072aa8ec
Dramvald <kirillpior@gmail.com>
2021-02-14 22:35:11 +0300

    changed the sort method

--- drink.py
+++ drink.py
@@ -1,59 +1,55 @@
 import requests
 import json
 from prettytable import PrettyTable
-import re
-
+from queue import PriorityQueue
 
 ALCOHOL = 'Alcoholic'
 RANDOM_DRINK_API_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
-INGREDIENT = r'^strIngredient.'
-MEASURE = r'^strMeasure.'
+INGREDIENT = 'strIngredient'
+MEASURE = 'strMeasure'
 
 
 def get_drink_data(RANDOM_DRINK_API_URL):
     """Делам запрос на сервер по адресу req_url.
     Преобразуем строку json в объект python типа dict."""
     data = requests.get(RANDOM_DRINK_API_URL).json()
-
-    def get_instructions_measures():
-        """Эта функция позволяет автоматически искать и заносит в список ингредиенты, и их количество"""
-        ingredient = []
-        measure = []
-        for key in data['drinks']:
-            for x, y in key.items():
-                if re.match(INGREDIENT, x):
-                    ingredient.append(y)
-                if re.match(MEASURE, x):
-                    measure.append(y)
-        return ingredient, measure
-    ingredients, measure = get_instructions_measures()
     # Так как содержимое ключа drinks имеет тип list,
     # использую for для того чтобы пройтись по элементам списка,
     # которые являются словарями, и взять нужные данные.
     for item in data['drinks']:
-        drink = item['strDrink']
+        drinks = item['strDrink']
         instruction = item['strInstructions']
-
-        # Уменьшаю список убирая в ingredients и measure элементы имеющие None.
-        ingredients = list(filter(None, ingredients))
-        measure = list(filter(None, measure))
         # Делаю проверку на алкогольный или без алкогольный напиток, и если он без алкогольный,
         # то отправляется снова запрос и проходит по циклу.
         if ALCOHOL != item['strAlcoholic']:
             return get_drink_data(RANDOM_DRINK_API_URL)
         else:
-            return drink, instruction, ingredients, measure
+            return drinks, instruction, data['drinks'][0]
 
 
-def show_drink(drink, instruction, ingredients, measure):
+def sort_values(drinks, property_name):
+    """Эта функция позволяет автоматически найти и записать данные искомых значений"""
+    pq = PriorityQueue()
+    for key, value in drinks.items():
+        if isinstance(key, str) and key.startswith(property_name):
+            if value is not None:
+                # Уменьшаю список убирая элементы имеющие None.
+                pq.put((int(key[-1]), value))
+    return [i[1] for i in pq.queue]
+
+
+def show_drink(drinks, instruction, ingredients, measure):
     """Функция для создания таблицы."""
     p = PrettyTable()
-    p.add_column('Drink name', [drink])
+    p.add_column('Drink name', [drinks])
     p.add_column('Drink sign', [ALCOHOL])
     p.add_column('Instructions for preparation', [instruction])
     p.add_column('Ingredients', [ingredients])
     p.add_column('Ingredient quantity', [measure])
     print(p.get_string())
 
-# drink, instruction, ingredients, measure = get_drink_data(req_url)
-# show_drink(drink, instruction, ingredients, measure)
+
+#drinks, instruction, data_drinks = get_drink_data(RANDOM_DRINK_API_URL)
+#ingredients = sort_values(data_drinks, INGREDIENT)
+#measure = sort_values(data_drinks, MEASURE)
+#show_drink(drinks, instruction, ingredients, measure)
